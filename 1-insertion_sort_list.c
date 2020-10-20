4f39302206797296256eac4fa1ac40597253568d
kali <kali@kali>
2020-10-20 11:20:07 -0400

    update

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -11,66 +11,40 @@
 
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *tmp = *list, *tmp1 = tmp->next;
-	int index = 0, i;
+	listint_t *tmp = *list, *curr, *left;
 
-	if (*list == NULL || list == NULL)
+	if (!list || !(*list)->next)
 		return;
-	if ((*list)->next == NULL && (*list)->prev == NULL)
-		return;
-	while (tmp1 != NULL)
+
+	curr = tmp->next;
+	while (curr)
 	{
-		if (tmp->n > tmp1->n)
+		left = curr;
+		while (left->prev)
 		{
-			tmp1->prev = tmp->prev;
-			tmp->next = tmp1->next;
-			tmp->prev = tmp1;
-			tmp1->next = tmp;
-			if (tmp1->prev == NULL)
+			tmp = left->prev;
+			if (tmp->n > left->n)
 			{
-				if (tmp->next != NULL)
-					tmp->next->prev = tmp;
-				*list = tmp1;
+
+				tmp->next = left->next;
+				if (!curr->next)
+					tmp->next = NULL;
+				else
+					left->next->prev = tmp;
+
+				if (!tmp->prev)
+					*list = left;
+				else
+					tmp->prev->next = left;
+				left->prev = tmp->prev;
+				left->next = tmp;
+				tmp->prev = left;
+				print_list(*list);
+				continue;
 			}
-			else if (tmp->next == NULL && tmp1->prev != NULL)
-				tmp1->prev->next = tmp1;
-			else if (tmp1->prev != NULL && tmp->next != NULL)
-			{
-				tmp->next->prev = tmp;
-				tmp1->prev->next = tmp1;
-			}
-			print_list(*list);
-			tmp = tmp1->prev;
-			while (tmp != NULL)
-			{
-				if (tmp->n > tmp1->n)
-				{
-					tmp1->prev = tmp->prev;
-					tmp->next = tmp1->next;
-					tmp->prev = tmp1;
-					tmp1->next = tmp;
-					if (tmp1->prev == NULL)
-					{
-						tmp->next->prev = tmp;
-						*list = tmp1;
-					}
-					else
-					{
-						tmp->next->prev = tmp;
-						tmp1->prev->next = tmp1;
-					}
-					print_list(*list);
-				}
-				tmp1 = tmp;
-				tmp = tmp->prev;
-			}
+			left = left->prev;
 		}
-		tmp = *list;
-		index += 1;
-		for (i = 0; index > i; i++)
-		{
-			tmp = tmp->next;
-			tmp1 = tmp->next;
-		}
+		curr = curr->next;
 	}
 }
+
